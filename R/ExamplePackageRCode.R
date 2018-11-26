2dfe5b3128a903d93468944cb7bbf7bdeae72a61
epicentered <amlbinder@gmail.com>
2018-11-25 23:12:22 -0800

    Initial Commit

---
+++ R/ExamplePackageRCode.R
@@ -0,0 +1,22 @@
+proportionofrolls<-function(Rolls=100,DiceSum=c(3,10,11)){
+  
+  d1<-c(1:6)
+  d2<-c(1:6)
+  
+  vectout<-rep(NA,Rolls)
+  
+  for(i in 1:Rolls){
+    
+    temp1<-sample(d1,1)
+    temp2<-sample(d2,1)
+    tempsum<-temp1+temp2
+    
+    if (tempsum %in% DiceSum) tempindictor<-1 else tempindictor<-0
+    vectout[i]<-tempindictor
+    
+  }
+  
+  proportionrolls<-sum(vectout)/Rolls
+  return(proportionrolls)
+  
+}
